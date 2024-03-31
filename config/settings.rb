6bdcc404fb479f73828e20f5b4f6b854cc0dbf18
Thiago Sawada <tms1_tms2@hotmail.com>
2024-03-31 18:50:58 -0300

    feat: settings

---
+++ config/settings.rb
@@ -0,0 +1,6 @@
+module Settings
+  SOURCE_GITLAB_TOKEN = 'glpat-ZM2_ebB6SBx4yNi-Pznw'
+  DESTINATION_GITLAB_TOKEN = 'glpat-ZM2_ebB6SBx4yNi-Pznw'
+  SOURCE_GITLAB_API_ENDPOINT = 'https://gitlab.com/api/v4'
+  DESTINATION_GITLAB_API_ENDPOINT = 'https://gitlab.com/api/v4'
+end

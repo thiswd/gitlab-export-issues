1a6c7cda2c56f1602bc6d628c58bbc2fb25603c7
Thiago Sawada <tms1_tms2@hotmail.com>
2024-04-02 15:58:13 -0300

    refactor: migration manager

--- lib/migration_manager.rb
+++ lib/migration_manager.rb
@@ -1,6 +1,7 @@
-require_relative 'gitlab_api/issue'
-require_relative 'gitlab_api/comment'
-require_relative 'gitlab_api/milestone'
+require_relative 'gitlab_api'
+require_relative 'issue_manager'
+require_relative 'milestone_manager'
+require_relative 'comment_manager'
 require_relative '../config/settings'
 
 class MigrationManager
@@ -8,15 +9,12 @@ class MigrationManager
     @source_project_id = source_project_id
     @destination_project_id = destination_project_id
 
-    # Source Managers
-    @source_issue_manager = GitLabAPI::Issue.new(Settings::SOURCE_GITLAB_TOKEN, Settings::SOURCE_GITLAB_API_ENDPOINT)
-    @source_milestone_manager = GitLabAPI::Milestone.new(Settings::SOURCE_GITLAB_TOKEN, Settings::SOURCE_GITLAB_API_ENDPOINT)
-    @source_comment_manager = GitLabAPI::Comment.new(Settings::SOURCE_GITLAB_TOKEN, Settings::SOURCE_GITLAB_API_ENDPOINT)
+    source_gitlab_api = GitlabApi.new(Settings::SOURCE_GITLAB_API_ENDPOINT,Settings::SOURCE_GITLAB_TOKEN)
+    destination_gitlab_api = GitlabApi.new(Settings::DESTINATION_GITLAB_API_ENDPOINT, Settings::DESTINATION_GITLAB_TOKEN)
 
-    # Destination Managers
-    @destination_issue_manager = GitLabAPI::Issue.new(Settings::DESTINATION_GITLAB_TOKEN, Settings::DESTINATION_GITLAB_API_ENDPOINT)
-    @destination_milestone_manager = GitLabAPI::Milestone.new(Settings::DESTINATION_GITLAB_TOKEN, Settings::DESTINATION_GITLAB_API_ENDPOINT)
-    @destination_comment_manager = GitLabAPI::Comment.new(Settings::DESTINATION_GITLAB_TOKEN, Settings::DESTINATION_GITLAB_API_ENDPOINT)
+    @milestone_manager = MilestoneManager.new(source_gitlab_api, destination_gitlab_api)
+    @issue_manager = IssueManager.new(source_gitlab_api, destination_gitlab_api)
+    @comment_manager = CommentManager.new(source_gitlab_api,destination_gitlab_api)
   end
 
   def migrate
@@ -28,34 +26,34 @@ class MigrationManager
   private
 
   def migrate_milestones
-    milestones = @source_milestone_manager.fetch_milestones(@source_project_id)
+    milestones = @milestone_manager.fetch_milestones(@source_project_id)
     milestones.each do |milestone|
-      next if @destination_milestone_manager.milestone_exists?(@destination_project_id, milestone['title'])
-      @destination_milestone_manager.create_milestone(@destination_project_id, milestone)
+      next if @milestone_manager.milestone_exists?(@destination_project_id, milestone['title'])
+      @milestone_manager.create_milestone(@destination_project_id, milestone)
     end
   end
 
   def migrate_issues
-    issues = @source_issue_manager.fetch_issues(@source_project_id)
+    issues = @issue_manager.fetch_issues(@source_project_id)
     issues.each do |issue|
-      next if @destination_issue_manager.issue_exists?(@destination_project_id, issue['title'])
-      created_issue_iid = @destination_issue_manager.create_issue(@destination_project_id, issue)
+      next if @issue_manager.issue_exists?(@destination_project_id, issue['title'])
+      created_issue_iid = @issue_manager.create_issue(@destination_project_id, issue)
 
       original_author = issue['author']['name']
       original_created_at = issue['created_at']
-      @source_comment_manager.create_initial_comment(@destination_project_id, created_issue_iid, original_author, original_created_at)
+      @comment_manager.create_initial_comment(@destination_project_id, created_issue_iid, original_author, original_created_at)
 
       migrate_comments(issue['iid'], created_issue_iid)
     end
   end
 
   def migrate_comments(source_issue_iid, destination_issue_iid)
-    comments = @source_comment_manager.fetch_comments(@source_project_id, source_issue_iid)
+    comments = @comment_manager.fetch_comments(@source_project_id, source_issue_iid)
     comments.each do |comment|
       original_author = comment['author']['name']
       original_created_at = comment['created_at']
       comment_body = comment['body']
-      @destination_comment_manager.create_comment(@destination_project_id, destination_issue_iid, comment_body, original_author, original_created_at)
+      @comment_manager.create_comment(@destination_project_id, destination_issue_iid, comment_body, original_author, original_created_at)
     end
   end
 end
